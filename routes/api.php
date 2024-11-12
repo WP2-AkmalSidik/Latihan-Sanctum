4c5d38491eafaa517087d034dcb4877962eb1ae5
Akmal Sidik <143138356+WP2-AkmalSidik@users.noreply.github.com>
2024-11-12 21:28:19 +0700

    refactor(route): change code placement for better

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
@@ -13,12 +16,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('userprofile', [AuthController::class, 'profile']);
     Route::get('logout', [AuthController::class, 'logout']);
 
-    Route::get('transactions', [TransactionController::class, 'index']);
-    Route::post('transactions', [TransactionController::class, 'store']);
-    Route::get('transactions/{id}', [TransactionController::class, 'show']);
-    Route::put('transactions/{id}', [TransactionController::class, 'update']);
-    Route::delete('transactions/{id}', [TransactionController::class, 'destroy']);
+    Route::get('/transactions', [TransactionController::class, 'index']);
+    Route::post('/transactions', [TransactionController::class, 'store']);
+    Route::get('/transactions/{id}', [TransactionController::class, 'show']);
+    Route::put('/transactions/{id}', [TransactionController::class, 'update']);
+    Route::delete('/transactions/{id}', [TransactionController::class, 'destroy']);
 });
-
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
